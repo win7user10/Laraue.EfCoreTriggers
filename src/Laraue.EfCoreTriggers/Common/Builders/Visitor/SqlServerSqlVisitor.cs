75c42a72c4fae4e9714b383248b93d11cee83767
Ilya Belyanskiy <ilya.belyanskiy@pm.bet>
2020-11-13 12:23:43 +0400

    Part of SQL generating for SQLServer added.

--- src/Laraue.EfCoreTriggers/Common/Builders/Visitor/SqlServerSqlVisitor.cs
+++ src/Laraue.EfCoreTriggers/Common/Builders/Visitor/SqlServerSqlVisitor.cs
@@ -22,13 +22,23 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Visitor
 
         public override GeneratedSql GetTriggerSql<TTriggerEntity>(Trigger<TTriggerEntity> trigger)
         {
-            var sqlBuilder = new GeneratedSql();
+            var actionsSql = trigger.Actions.Select(action => action.BuildSql(this));
+
+            var sqlBuilder = new GeneratedSql(actionsSql);
             sqlBuilder.Append($"CREATE TRIGGER {trigger.Name} ON {GetTableName(typeof(TTriggerEntity))}")
                 .Append($" FOR {trigger.TriggerType} AS BEGIN ");
 
-            sqlBuilder.Append(DeclareCursorBlocksSql(trigger.TriggerType, sqlBuilder.AffectedColumns));
-            sqlBuilder.Append(FetchCursorsSql(sqlBuilder.AffectedColumns))
-                .Append("WHILE @@FETCH_STATUS = 0");
+            sqlBuilder.Append(DeclareCursorBlocksSql(trigger.TriggerType, sqlBuilder.AffectedColumns))
+                .Append(" ")
+                .Append(FetchCursorsSql(sqlBuilder.AffectedColumns))
+                .Append(" WHILE @@FETCH_STATUS = 0")
+                .Append(" BEGIN ")
+                .AppendJoin(actionsSql.Select(x => x.SqlBuilder))
+                .Append(FetchCursorsSql(sqlBuilder.AffectedColumns))
+                .Append(" END ");
+
+            sqlBuilder.Append(CloseCursorsBlockSql(sqlBuilder.AffectedColumns))
+                .Append(" END");
 
             return sqlBuilder;
         }
@@ -69,16 +79,28 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Visitor
             => $"DECLARE {cursorName} CURSOR FOR";
 
         private string FetchCursorsSql(Dictionary<ArgumentPrefix, HashSet<MemberInfo>> members)
-            => string.Join(" ", members.Select(x => FetchCursorSql(x.Key, x.Value)));
+            => string.Join(" ", members.Where(x => x.Value.Count > 0).Select(x => FetchCursorSql(x.Key, x.Value)));
 
         private string FetchCursorSql(ArgumentPrefix argumentPrefix, IEnumerable<MemberInfo> members)
             => $"FETCH NEXT FROM {CursorName(argumentPrefix)} INTO {string.Join(", ", members.Select(member => VariableNameSql(argumentPrefix, member)))}";
 
-        private string SelectFromCursorSql(TriggerType triggerType, IEnumerable<MemberInfo> members)
-            => $"SELECT {string.Join(", ", members.Select(x => GetColumnName(x)))} FROM {triggerType.ToString().ToUpper()}";
+        private string SelectFromCursorSql(ArgumentPrefix argumentPrefix, IEnumerable<MemberInfo> members)
+            => $"SELECT {string.Join(", ", members.Select(x => GetColumnName(x)))} FROM {CursorName(argumentPrefix)}";
 
         private string DeclareCursorVariablesSql(ArgumentPrefix argumentPrefix, IEnumerable<MemberInfo> members)
-            => $"DECLARE {string.Join(", ", members.Select(member => VariableNameSql(argumentPrefix, member)))}";
+            => $"DECLARE {string.Join(", ", members.Select(member => DeclareVariableNameSql(argumentPrefix, member)))}";
+
+        private string CloseCursorSql(string cursorName)
+            => $"CLOSE {cursorName}";
+
+        private string DeallocateCursorSql(string cursorName)
+            => $"DEALLOCATE {cursorName}";
+
+        private string CloseCursorsBlockSql(Dictionary<ArgumentPrefix, HashSet<MemberInfo>> members)
+        {
+            return string.Join(" ", members.Where(x => x.Value.Count > 0)
+                .Select(x => $"{CloseCursorSql(CursorName(x.Key))} {DeallocateCursorSql(CursorName(x.Key))}"));
+        }
 
         private string VariableNameSql(ArgumentPrefix argumentPrefix, MemberInfo member)
         {
@@ -89,11 +111,27 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Visitor
                 _ => throw new InvalidOperationException($"Invalid attempt to generate declaring variable SQL using argument prefix {argumentPrefix}")
             };
 
-            return $"@{argumentPrefixString} {GetSqlServerType(member)}";
+            return $"@{argumentPrefixString}{member.Name}";
         }
 
-        private string GetSqlServerType(MemberInfo memberInfo)
-            => "int";
+        private string DeclareVariableNameSql(ArgumentPrefix argumentPrefix, MemberInfo member)
+            => $"{VariableNameSql(argumentPrefix, member)} {GetSqlServerType((PropertyInfo)member)}";
+
+        private string GetSqlServerType(PropertyInfo propertyInfo)
+        {
+            var mapping = new Dictionary<Type, string>
+            {
+                [typeof(bool)] = "bit",
+                [typeof(Guid)] = "uniqueidentifier",
+                [typeof(decimal)] = "decimal",
+                [typeof(int)] = "integer",
+                [typeof(string)] = "text",
+            };
+
+            if (mapping.TryGetValue(propertyInfo.PropertyType, out var type))
+                return type;
+            throw new NotSupportedException($"Unknown data type {propertyInfo.PropertyType}");
+        }
 
         private GeneratedSql DeclareCursorBlocksSql(TriggerType triggerType, Dictionary<ArgumentPrefix, HashSet<MemberInfo>> affectedMemberPairs)
         {
@@ -109,14 +147,16 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Visitor
             var cursorName = CursorName(argumentPrefix);
             return new GeneratedSql()
                 .Append(DeclareCursorSql(cursorName))
-                .Append("SELECT ")
-                .Append(SelectFromCursorSql(triggerType, affectedMembers))
+                .Append(" ")
+                .Append(SelectFromCursorSql(argumentPrefix, affectedMembers))
+                .Append(" ")
                 .Append(DeclareCursorVariablesSql(argumentPrefix, affectedMembers))
                 .Append($" OPEN {cursorName}");
         }
 
         public override GeneratedSql GetTriggerUpsertActionSql<TTriggerEntity, TUpsertEntity>(TriggerUpsertAction<TTriggerEntity, TUpsertEntity> triggerUpsertAction)
         {
+            return new GeneratedSql();
             throw new NotImplementedException();
         }
     }
