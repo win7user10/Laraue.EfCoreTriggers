e03714f2266407651d73f0131be91ae794441b97
Ilya Belyanskiy <win7user10@yandex.ru>
2021-08-14 11:42:50 +0300

    refactoring: Separated class for communication with EFCore model

--- src/Laraue.EfCoreTriggers/Common/Builders/Providers/BaseExpressionProvider.cs
+++ src/Laraue.EfCoreTriggers/Common/Builders/Providers/BaseExpressionProvider.cs
@@ -8,28 +8,8 @@ using System.Linq;
 
 namespace Laraue.EfCoreTriggers.Common.Builders.Providers
 {
-    public abstract class BaseExpressionProvider
+    public abstract class BaseExpressionProvider : EfCoreMetadataRetriever
     {
-        /// <summary>
-        /// Cached column names for entity properties.
-        /// </summary>
-        private readonly Dictionary<MemberInfo, string> _columnNamesCache = new();
-
-        /// <summary>
-        /// Cached table names for entities.
-        /// </summary>
-        private readonly Dictionary<Type, string> _tableNamesCache = new();
-
-        /// <summary>
-        /// Cached database schema names.
-        /// </summary>
-        private readonly Dictionary<Type, string> _tableSchemasCache = new();
-
-        /// <summary>
-        /// Model used for generating SQL. From this model takes column names, table names and other meta information.
-        /// </summary>
-        protected IModel Model { get; }
-
         /// <summary>
         /// Prefix for inserted or updated (new value) entity in triggers. E.g to get balance from inserted entity, 
         /// inPostgreSQL should be used syntax NEW.balance.
@@ -54,38 +34,6 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Providers
         protected abstract Dictionary<Type, string> TypeMappings { get; }
 
         /// <summary>
-        /// Initializes new instance of <see cref="BaseExpressionProvider"/>.
-        /// </summary>
-        /// <param name="model"></param>
-		public BaseExpressionProvider(IModel model)
-        {
-            Model = model ?? throw new ArgumentNullException(nameof(model));
-        }
-
-        /// <summary>
-        /// Get column name for passed property type.
-        /// </summary>
-        /// <param name="memberInfo"></param>
-        /// <returns></returns>
-        protected virtual string GetColumnName(MemberInfo memberInfo)
-        {
-            if (!_columnNamesCache.ContainsKey(memberInfo))
-            {
-                var entityType = Model.FindEntityType(memberInfo.DeclaringType);
-                var property = entityType.FindProperty(memberInfo.Name);
-                var identifier = (StoreObjectIdentifier)StoreObjectIdentifier.Create(entityType, StoreObjectType.Table);
-                _columnNamesCache.Add(memberInfo, property.GetColumnName(identifier));
-            }
-
-            if (!_columnNamesCache.TryGetValue(memberInfo, out var columnName))
-            {
-                throw new InvalidOperationException($"Column name for member {memberInfo.Name} is not defined in model");
-            }
-
-            return columnName;
-        }
-
-        /// <summary>
         /// Get SQL type represents passed <see cref="Type">CLR Type</see>.
         /// Based on <see cref="TypeMappings"/> but uses additional logic and all types should be 
         /// asked through this method.
@@ -98,59 +46,6 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Providers
             TypeMappings.TryGetValue(type, out var sqlType);
             return sqlType;
 		}
-
-        /// <summary>
-        /// Get table name in which situated entity with passed property.
-        /// </summary>
-        /// <param name="memberInfo"></param>
-        /// <returns></returns>
-        protected virtual string GetTableName(MemberInfo memberInfo) => GetTableName(memberInfo.DeclaringType);
-
-        /// <summary>
-        /// Get table name for passed <see cref="Type">ClrType</see>.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns></returns>
-        protected virtual string GetTableName(Type entity)
-        {
-            if (!_tableNamesCache.ContainsKey(entity))
-            {
-                var entityType = Model.FindEntityType(entity);
-                _tableNamesCache.Add(entity, entityType.GetTableName());
-            }
-
-            if (!_tableNamesCache.TryGetValue(entity, out var tableName))
-			{
-                throw new InvalidOperationException($"Table name for entity {entity.FullName} is not defined in model.");
-            }
-
-            var schemaName = GetTableSchemaName(entity);
-
-            return string.IsNullOrWhiteSpace(schemaName)
-                ? tableName
-                : $"{schemaName}.{tableName}";
-        }
-
-        /// <summary>
-        /// Get schema name for passed <see cref="Type">ClrType</see>.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns></returns>
-        protected virtual string GetTableSchemaName(Type entity)
-        {
-            if (!_tableSchemasCache.ContainsKey(entity))
-            {
-                var entityType = Model.FindEntityType(entity);
-                _tableSchemasCache.Add(entity, entityType.GetSchema());
-            }
-
-            if (!_tableSchemasCache.TryGetValue(entity, out var schemaName))
-            {
-                throw new InvalidOperationException($"Schema for entity {entity.FullName} is not defined in model.");
-            }
-
-            return schemaName;
-        }
 
         /// <summary>
         /// Get expression operand based on <see cref="ExpressionType"/> property.
@@ -438,5 +333,10 @@ namespace Laraue.EfCoreTriggers.Common.Builders.Providers
         /// <param name="value"></param>
         /// <returns></returns>
         protected virtual string GetBooleanSqlValue(bool value) => $"{value.ToString().ToLower()}";
+
+        /// <inheritdoc />
+        protected BaseExpressionProvider(IModel model) : base(model)
+        {
+        }
     }
 }
---
+++ src/Laraue.EfCoreTriggers/Common/Builders/Providers/EfCoreConnector.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Laraue.EfCoreTriggers.Common.Builders.Providers
+{
+    /// <summary>
+    /// Retrieve from model information about column names, table names, db schema e t.c.
+    /// </summary>
+    public class EfCoreMetadataRetriever
+    {
+        /// <summary>
+        /// Cached column names for entity properties.
+        /// </summary>
+        private readonly Dictionary<MemberInfo, string> _columnNamesCache = new();
+
+        /// <summary>
+        /// Cached table names for entities.
+        /// </summary>
+        private readonly Dictionary<Type, string> _tableNamesCache = new();
+
+        /// <summary>
+        /// Cached database schema names.
+        /// </summary>
+        private readonly Dictionary<Type, string> _tableSchemasCache = new();
+
+        /// <summary>
+        /// Model used for generating SQL. From this model takes column names, table names and other meta information.
+        /// </summary>
+        protected IModel Model { get; }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="BaseExpressionProvider"/>.
+        /// </summary>
+        /// <param name="model"></param>
+        public EfCoreMetadataRetriever(IModel model)
+        {
+            Model = model ?? throw new ArgumentNullException(nameof(model));
+        }
+
+        /// <summary>
+        /// Get column name for passed property type.
+        /// </summary>
+        /// <param name="memberInfo"></param>
+        /// <returns></returns>
+        protected virtual string GetColumnName(MemberInfo memberInfo)
+        {
+            if (!_columnNamesCache.ContainsKey(memberInfo))
+            {
+                var entityType = Model.FindEntityType(memberInfo.DeclaringType);
+                var property = entityType.FindProperty(memberInfo.Name);
+                var identifier = (StoreObjectIdentifier)StoreObjectIdentifier.Create(entityType, StoreObjectType.Table);
+                _columnNamesCache.Add(memberInfo, property.GetColumnName(identifier));
+            }
+
+            if (!_columnNamesCache.TryGetValue(memberInfo, out var columnName))
+            {
+                throw new InvalidOperationException($"Column name for member {memberInfo.Name} is not defined in model");
+            }
+
+            return columnName;
+        }
+
+        /// <summary>
+        /// Get table name in which situated entity with passed property.
+        /// </summary>
+        /// <param name="memberInfo"></param>
+        /// <returns></returns>
+        protected virtual string GetTableName(MemberInfo memberInfo) => GetTableName(memberInfo.DeclaringType);
+
+
+        /// <summary>
+        /// Get table name for passed <see cref="Type">ClrType</see>.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        protected virtual string GetTableName(Type entity)
+        {
+            if (!_tableNamesCache.ContainsKey(entity))
+            {
+                var entityType = Model.FindEntityType(entity);
+                _tableNamesCache.Add(entity, entityType.GetTableName());
+            }
+
+            if (!_tableNamesCache.TryGetValue(entity, out var tableName))
+            {
+                throw new InvalidOperationException($"Table name for entity {entity.FullName} is not defined in model.");
+            }
+
+            var schemaName = GetTableSchemaName(entity);
+
+            return string.IsNullOrWhiteSpace(schemaName)
+                ? tableName
+                : $"{schemaName}.{tableName}";
+        }
+
+        /// <summary>
+        /// Get schema name for passed <see cref="Type">ClrType</see>.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        protected virtual string GetTableSchemaName(Type entity)
+        {
+            if (!_tableSchemasCache.ContainsKey(entity))
+            {
+                var entityType = Model.FindEntityType(entity);
+                _tableSchemasCache.Add(entity, entityType.GetSchema());
+            }
+
+            if (!_tableSchemasCache.TryGetValue(entity, out var schemaName))
+            {
+                throw new InvalidOperationException($"Schema for entity {entity.FullName} is not defined in model.");
+            }
+
+            return schemaName;
+        }
+    }
+}
