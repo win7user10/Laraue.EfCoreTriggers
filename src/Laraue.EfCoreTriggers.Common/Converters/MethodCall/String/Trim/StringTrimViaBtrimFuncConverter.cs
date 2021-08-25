10bcddf8b272599a74fa16156ca38dabe89444ec
Waimagi <shitova.ap@spbgut.ru>
2021-08-25 12:00:02 +0300

    refactoring: base class for trim convectors

--- src/Laraue.EfCoreTriggers.Common/Converters/MethodCall/String/Trim/StringTrimViaBtrimFuncConverter.cs
+++ src/Laraue.EfCoreTriggers.Common/Converters/MethodCall/String/Trim/StringTrimViaBtrimFuncConverter.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq.Expressions;
-using Laraue.EfCoreTriggers.Common.SqlGeneration;
-using Laraue.EfCoreTriggers.Common.TriggerBuilders;
-
-namespace Laraue.EfCoreTriggers.Common.Converters.MethodCall.String.Trim
+﻿namespace Laraue.EfCoreTriggers.Common.Converters.MethodCall.String.Trim
 {
-    public class StringTrimViaBtrimFuncConverter : BaseStringConverter
+    public class StringTrimViaBtrimFuncConverter : BaseStringTrimConverter
     {
-        /// <inheritdoc />
-        public override string MethodName => nameof(string.Trim);
-
-        /// <inheritdoc />
-        public override SqlBuilder BuildSql(BaseExpressionProvider provider, MethodCallExpression expression, Dictionary<string, ArgumentType> argumentTypes)
-        {
-            var sqlBuilder = provider.GetExpressionSql(expression.Object, argumentTypes);
-            return new(sqlBuilder.AffectedColumns, $"BTRIM({sqlBuilder})");
-        }
+        public override string TrimsTypeName => "BTRIM";
     }
 }
