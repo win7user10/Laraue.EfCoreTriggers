aaa5587f7b530b6fd7b3ccb74d85a4088a4e3c90
Waimagi <shitova.ap@spbgut.ru>
2021-08-25 12:57:26 +0300

    feat: method call was edited like a base class

--- src/Laraue.EfCoreTriggers.Common/Converters/MethodCall/MethodCallConverter.cs
+++ src/Laraue.EfCoreTriggers.Common/Converters/MethodCall/MethodCallConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Laraue.EfCoreTriggers.Common.SqlGeneration;
 using Laraue.EfCoreTriggers.Common.TriggerBuilders;
@@ -7,8 +8,13 @@ namespace Laraue.EfCoreTriggers.Common.Converters.MethodCall
 {
     public abstract class MethodCallConverter : IMethodCallConverter
     {
+        public abstract string MethodName { get; }
+        public abstract  Type ReflectedType { get; }
         /// <inheritdoc />
-        public abstract bool IsApplicable(MethodCallExpression expression);
+        public bool IsApplicable(MethodCallExpression expression)
+        {
+            return expression.Method.ReflectedType == ReflectedType && MethodName == expression.Method.Name;
+        }
 
         /// <inheritdoc />
         public abstract SqlBuilder BuildSql(BaseExpressionProvider provider, MethodCallExpression expression,
