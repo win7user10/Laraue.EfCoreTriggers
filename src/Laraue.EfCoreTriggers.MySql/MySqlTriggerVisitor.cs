1190bf0c379ad99ad4c5005db996adfa93b3eedc
Ilya Belyanskiy <win7user20@gmail.com>
2022-11-14 13:06:02 +0300

    upd all dependencies besides MySql

--- src/Laraue.EfCoreTriggers.MySql/MySqlTriggerVisitor.cs
+++ src/Laraue.EfCoreTriggers.MySql/MySqlTriggerVisitor.cs
@@ -4,6 +4,7 @@ using Laraue.EfCoreTriggers.Common.Services.Impl.TriggerVisitors;
 using Laraue.EfCoreTriggers.Common.SqlGeneration;
 using Laraue.EfCoreTriggers.Common.TriggerBuilders.Base;
 using Microsoft.EntityFrameworkCore.Metadata;
+using ITrigger = Laraue.EfCoreTriggers.Common.TriggerBuilders.Base.ITrigger;
 
 namespace Laraue.EfCoreTriggers.MySql;
 
