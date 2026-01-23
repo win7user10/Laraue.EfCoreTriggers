59090710fd6df327d46be6b2123eb233a967b784
Ilya Belyansky <win7user10@yandex.ru>
2026-01-22 22:36:54 -0800

    initialize oracle test project

---
+++ tests/Laraue.EfCoreTriggers.OracleTests/Unit/OracleUnitMemberAssignmentTests.cs
@@ -0,0 +1,41 @@
+﻿using Laraue.EfCoreTriggers.Oracle.Extensions;
+using Laraue.EfCoreTriggers.Tests;
+using Laraue.EfCoreTriggers.Tests.Infrastructure;
+using Laraue.EfCoreTriggers.Tests.Tests.Unit;
+using Xunit;
+
+namespace Laraue.EfCoreTriggers.OracleTests.Unit
+{
+    [Collection(CollectionNames.SqlServer)]
+    public class OracleUnitMemberAssignmentTests : BaseMemberAssignmentUnitTests
+    {
+        public OracleUnitMemberAssignmentTests() : base(
+            Helper.GetTriggerActionFactory(
+                new ContextFactory().CreateDbContext().Model, 
+                collection => collection.AddOracleServices()))
+        {
+        }
+        
+        public override string ExceptedEnumValueSql => "INSERT INTO \"DestinationEntities\" (\"EnumValue\") SELECT @NewEnumValue;";
+
+        public override string ExceptedDecimalAddSql => "INSERT INTO \"DestinationEntities\" (\"DecimalValue\") SELECT @NewDecimalValue + 3;";
+
+        public override string ExceptedDoubleSubSql => "INSERT INTO \"DestinationEntities\" (\"DoubleValue\") SELECT @NewDoubleValue + 3;";
+
+        public override string ExceptedIntMultiplySql => "INSERT INTO \"DestinationEntities\" (\"IntValue\") SELECT @NewIntValue * 2;";
+
+        public override string ExceptedBooleanSql => "INSERT INTO \"DestinationEntities\" (\"BooleanValue\") SELECT CASE WHEN @NewBooleanValue = 0 THEN 1 ELSE 0 END;";
+
+        public override string ExceptedNewGuidSql => "INSERT INTO \"DestinationEntities\" (\"GuidValue\") SELECT NEWID();";
+
+        public override string ExceptedCharVariableSql => "INSERT INTO \"DestinationEntities\" (\"CharValue\") SELECT @NewCharValue;";
+
+        public override string ExceptedCharValueSql => "INSERT INTO \"DestinationEntities\" (\"CharValue\") SELECT 'a';";
+        public override string ExceptedDateTimeOffsetNowSql => "INSERT INTO \"DestinationEntities\" (\"DateTimeOffsetValue\") SELECT SYSDATETIME();";
+        public override string ExceptedDateTimeOffsetUtcNowSql => "INSERT INTO \"DestinationEntities\" (\"DateTimeOffsetValue\") SELECT SYSUTCDATETIME();";
+
+        public override string ExceptedNewDateTimeOffsetSql => "INSERT INTO \"DestinationEntities\" (\"DateTimeOffsetValue\") SELECT '1753-01-01';";
+
+        public override string ExceptedNewDateTimeSql => "INSERT INTO \"DestinationEntities\" (\"DateTimeValue\") SELECT '1753-01-01';";
+    }
+}
